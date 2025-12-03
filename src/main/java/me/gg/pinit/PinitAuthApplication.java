0d27ed16bb357594d344c83f615830ceb7d7f81d
GoGradually <chkun3109@gmail.com>
2025-12-03 14:11:32 +0900

    build: 의존성 추가 및 구성 변경

--- src/main/java/me/gg/pinit/PinitAuthApplication.java
+++ src/main/java/me/gg/pinit/PinitAuthApplication.java
@@ -2,6 +2,7 @@ package me.gg.pinit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
 public class PinitAuthApplication {
