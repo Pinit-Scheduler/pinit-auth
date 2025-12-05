e00120a1d4301da322f5a05de32dce9d5bfcbb1d
GoGradually <chkun3109@gmail.com>
2025-12-05 13:45:06 +0900

    feat: OpenIdTokenRequest 클래스 추가

---
+++ src/main/java/me/gg/pinit/controller/dto/OpenIdTokenRequest.java
@@ -0,0 +1,16 @@
+package me.gg.pinit.controller.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OpenIdTokenRequest {
+    private String grant_type;
+    private String client_id;
+    private String client_secret;
+    private String code;
+    private String state;
+    private String refresh_token;
+    private String access_token;
+}
