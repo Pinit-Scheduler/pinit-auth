fbf574e34cdd0f57f47697129057de8fd9fb4aa4
GoGradually <chkun3109@gmail.com>
2025-12-05 17:42:30 +0900

    feat: OpenIdTokenRequest 클래스에 OpenIdCommand 기반의 정적 메서드 추가

--- src/main/java/me/gg/pinit/infra/dto/OpenIdTokenRequest.java
+++ src/main/java/me/gg/pinit/infra/dto/OpenIdTokenRequest.java
@@ -2,6 +2,10 @@ package me.gg.pinit.infra.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import me.gg.pinit.domain.oidc.OpenIdCommand;
+import me.gg.pinit.domain.oidc.OpenIdPublishCommand;
+import me.gg.pinit.domain.oidc.OpenIdRefreshCommand;
+import me.gg.pinit.domain.oidc.OpenIdRevokeCommand;
 
 @Getter
 @Setter
@@ -13,4 +17,24 @@ public class OpenIdTokenRequest {
     private String state;
     private String refresh_token;
     private String access_token;
+    private String service_provider;
+
+    public static OpenIdTokenRequest from(OpenIdCommand command, String client_id, String client_secret, String service_provider) {
+        OpenIdTokenRequest request = new OpenIdTokenRequest();
+        request.setGrant_type(command.getGrantType());
+        request.setClient_id(client_id);
+        request.setClient_secret(client_secret);
+        if (command instanceof OpenIdRefreshCommand refreshCommand) {
+            request.setRefresh_token((refreshCommand.getRefreshToken()));
+        }
+        if (command instanceof OpenIdPublishCommand publishCommand) {
+            request.setCode(publishCommand.getCode());
+            request.setState(publishCommand.getState());
+        }
+        if (command instanceof OpenIdRevokeCommand revokeCommand) {
+            request.setAccess_token(revokeCommand.getAccessToken());
+            request.setService_provider(service_provider);
+        }
+        return request;
+    }
 }
