c5fc4049a9cdd808534e40792553b131b3ec71e3
GoGradually <chkun3109@gmail.com>
2025-12-05 14:45:56 +0900

    feat: NaverOauth2Provider 인터페이스에 Oauth2Provider 상속 및 getToken 메서드 수정

--- src/main/java/me/gg/pinit/domain/oidc/naver/NaverOauth2Provider.java
+++ src/main/java/me/gg/pinit/domain/oidc/naver/NaverOauth2Provider.java
@@ -1,10 +1,12 @@
 package me.gg.pinit.domain.oidc.naver;
 
+import me.gg.pinit.domain.oidc.Oauth2Provider;
 import me.gg.pinit.domain.oidc.OpenIdCommand;
 import me.gg.pinit.domain.oidc.Profile;
 
-public interface NaverOauth2Provider {
-    NaverOauth2Token getToken(OpenIdCommand command);
+import java.util.List;
 
+public interface NaverOauth2Provider extends Oauth2Provider {
+    List<NaverOauth2Token> getToken(OpenIdCommand command);
     Profile getProfile(OpenIdCommand command);
 }
