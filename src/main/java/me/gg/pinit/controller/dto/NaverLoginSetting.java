a6d4f0a692307e0913ecfa59b13703b73f105ede
GoGradually <chkun3109@gmail.com>
2025-12-05 13:44:36 +0900

    feat: NaverLoginSetting 클래스 추가

---
+++ src/main/java/me/gg/pinit/controller/dto/NaverLoginSetting.java
@@ -0,0 +1,8 @@
+package me.gg.pinit.controller.dto;
+
+public class NaverLoginSetting {
+    private String response_type;
+    private String client_id;
+    private String redirect_uri;
+    private String state;
+}
