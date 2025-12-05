1d688b04d292c67031e1fd101a7233bd2f8a7835
GoGradually <chkun3109@gmail.com>
2025-12-05 13:45:43 +0900

    feat: OpenIdTokenResponse 클래스 추가

---
+++ src/main/java/me/gg/pinit/controller/dto/OpenIdTokenResponse.java
@@ -0,0 +1,11 @@
+package me.gg.pinit.controller.dto;
+
+public class OpenIdTokenResponse {
+    private String refresh_token;
+    private String access_token;
+    private String token_type;
+    private String result;
+    private int expires_in;
+    private String error;
+    private String error_description;
+}
