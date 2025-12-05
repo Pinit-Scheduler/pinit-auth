7786a399a65a41772017b09edd277d284299cfd5
GoGradually <chkun3109@gmail.com>
2025-12-05 19:29:35 +0900

    feat: NaverClient에 프로필 조회 메소드 추가 및 RestClient 분리

--- src/main/java/me/gg/pinit/infra/naver/NaverClient.java
+++ src/main/java/me/gg/pinit/infra/naver/NaverClient.java
@@ -1,26 +1,36 @@
 package me.gg.pinit.infra.naver;
 
 import me.gg.pinit.domain.oidc.OpenIdCommand;
+import me.gg.pinit.domain.oidc.Profile;
+import me.gg.pinit.infra.dto.OpenIdProfileResponse;
 import me.gg.pinit.infra.dto.OpenIdTokenRequest;
 import me.gg.pinit.infra.dto.OpenIdTokenResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.client.RestClient;
 
+import java.util.Objects;
+
 public class NaverClient {
     private static final String OAUTH_2_0_TOKEN = "/oauth2.0/token";
-    private final RestClient restClient;
+    public static final String NAVER_ID_URL = "https://nid.naver.com";
+    public static final String NAVER_APP_URL = "https://openapi.naver.com";
+    private final RestClient loginClient;
+    private final RestClient profileClient;
     private final NaverRegistrationProperties naverRegistrationProperties;
 
     public NaverClient(NaverRegistrationProperties naverRegistrationProperties) {
-        restClient = RestClient.builder()
-                .baseUrl("https://nid.naver.com").defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        loginClient = RestClient.builder()
+                .baseUrl(NAVER_ID_URL).defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+        profileClient = RestClient.builder()
+                .baseUrl(NAVER_APP_URL).defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
         this.naverRegistrationProperties = naverRegistrationProperties;
     }
 
     public OpenIdTokenResponse getToken(OpenIdCommand command) {
-        return restClient.post()
+        return loginClient.post()
                 .uri(OAUTH_2_0_TOKEN)
                 .body(OpenIdTokenRequest.from(
                         command,
@@ -29,6 +39,16 @@ public class NaverClient {
                         naverRegistrationProperties.getProvider()))
                 .retrieve()
                 .body(OpenIdTokenResponse.class);
+    }
+
+    public Profile getProfile(String accessToken) {
+        OpenIdProfileResponse result = profileClient.get()
+                .uri("/v1/nid/me")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                .retrieve()
+                .body(OpenIdProfileResponse.class);
+        Objects.requireNonNull(result);
+        return new Profile(result.getSub(), result.getNickname());
     }
 
 }
