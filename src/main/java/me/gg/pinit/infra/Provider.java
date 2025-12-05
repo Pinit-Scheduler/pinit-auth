46075b12c81712477e55d49ab63941f632cd7663
GoGradually <chkun3109@gmail.com>
2025-12-05 21:22:33 +0900

    feat: Provider 어노테이션 클래스 추가

---
+++ src/main/java/me/gg/pinit/infra/Provider.java
@@ -0,0 +1,15 @@
+package me.gg.pinit.infra;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Component
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Provider {
+    String value() default "";
+}
