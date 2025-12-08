0e0a185dcb9bb91e09c2b6b02366fe825bbeb714
GoGradually <chkun3109@gmail.com>
2025-12-08 23:39:54 +0900

    feat: RabbitMQ 설정에서 도메인 이벤트 퍼블리셔 추가

--- src/main/java/me/gg/pinit/infra/config/RabbitMQConfig.java
+++ src/main/java/me/gg/pinit/infra/config/RabbitMQConfig.java
@@ -1,25 +1,28 @@
 package me.gg.pinit.infra.config;
 
+import me.gg.pinit.domain.event.DomainEventPublisher;
+import me.gg.pinit.infra.events.AmqpEventMapper;
+import me.gg.pinit.infra.events.MemberMessaging;
+import me.gg.pinit.infra.events.RabbitDomainEventPublisher;
 import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.FanoutExchange;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class RabbitMQConfig {
     @Bean
-    public FanoutExchange memberFanout() {
-        return new FanoutExchange("auth.member.fanout");
-    }
-
-    @Bean
     public DirectExchange memberDirect() {
-        return new DirectExchange("auth.member.direct");
+        return new DirectExchange(MemberMessaging.DIRECT_EXCHANGE);
     }
 
     @Bean
-    public TopicExchange memberTopic() {
-        return new TopicExchange("auth.member.topic");
+    public DomainEventPublisher domainEventPublisher(RabbitTemplate rabbitTemplate,
+                                                     List<AmqpEventMapper<?>> mapperList) {
+        // mapperList에는 스프링이 관리하는 모든 AmqpEventMapper 구현체가 주입된다.
+        // 이는 스프링 DI의 기능 중 하나이다.
+        return new RabbitDomainEventPublisher(rabbitTemplate, mapperList);
     }
 }
