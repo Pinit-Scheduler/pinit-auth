7c1707eabf91d0c897c36c742450685fc06dfe53
GoGradually <chkun3109@gmail.com>
2025-12-15 19:49:40 +0900

    feat: 오버라이딩을 통한 exchange, routing key 탐색

--- src/main/java/me/gg/pinit/infra/events/outbox/Outbox.java
+++ src/main/java/me/gg/pinit/infra/events/outbox/Outbox.java
@@ -1,13 +1,19 @@
-package me.gg.pinit.infra.events;
+package me.gg.pinit.infra.events.outbox;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn
+@Getter
 public abstract class Outbox {
 
     @Id
     @GeneratedValue
     private Long id;
+
+    public abstract String exchange();
+
+    public abstract String routingKey();
 }
