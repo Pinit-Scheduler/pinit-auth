d83c6c37c91167f103c1e1d6e41e42a9ceeb851f
GoGradually <chkun3109@gmail.com>
2025-12-15 19:51:06 +0900

    refactor: MemberMessaging 패키지 경로 변경

--- src/main/java/me/gg/pinit/infra/config/RabbitMQConfig.java
+++ src/main/java/me/gg/pinit/infra/config/RabbitMQConfig.java
@@ -1,6 +1,6 @@
 package me.gg.pinit.infra.config;
 
-import me.gg.pinit.infra.events.MemberMessaging;
+import me.gg.pinit.infra.events.member.MemberMessaging;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
