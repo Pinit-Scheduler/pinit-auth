cbf93286640a102927af3163c549c859ec6c3bf7
GoGradually <chkun3109@gmail.com>
2025-12-20 20:34:53 +0900

    refactor: 테스트에서 JwtTokenProvider를 Mockito로 대체

--- src/test/java/me/gg/pinit/PinitAuthApplicationTests.java
+++ src/test/java/me/gg/pinit/PinitAuthApplicationTests.java
@@ -1,30 +1,20 @@
 package me.gg.pinit;
 
 import me.gg.pinit.infrastructure.jwt.JwtTokenProvider;
-import me.gg.pinit.utils.TestKeys;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.ActiveProfiles;
-
-import java.time.Duration;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @ActiveProfiles("test")
 @SpringBootTest
 class PinitAuthApplicationTests {
 
+    @MockitoBean
+    JwtTokenProvider jwtTokenProvider;
+
+
     @Test
     void contextLoads() {
-    }
-
-    @TestConfiguration
-    static class TestBeans {
-        @Bean
-        @Primary
-        JwtTokenProvider testJwtTokenProvider() {
-            return new JwtTokenProvider(TestKeys.privateKey(), TestKeys.publicKey(), "https://pinit.go-gradually.me", Duration.ofMinutes(5), Duration.ofDays(14));
-        }
     }
 }
