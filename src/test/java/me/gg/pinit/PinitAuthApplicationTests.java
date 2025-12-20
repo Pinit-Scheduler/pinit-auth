50140f2b05a2459cd95bcaa7dea1243b3d955339
GoGradually <chkun3109@gmail.com>
2025-12-20 20:14:55 +0900

    test: 테스트 환경을 위한 JWT 키 생성기 추가

--- src/test/java/me/gg/pinit/PinitAuthApplicationTests.java
+++ src/test/java/me/gg/pinit/PinitAuthApplicationTests.java
@@ -1,8 +1,17 @@
 package me.gg.pinit;
 
+import me.gg.pinit.infrastructure.jwt.JwtTokenProvider;
+import me.gg.pinit.utils.TestKeys;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.test.context.ActiveProfiles;
 
+import java.time.Duration;
+
+@ActiveProfiles("test")
 @SpringBootTest
 class PinitAuthApplicationTests {
 
@@ -10,4 +19,12 @@ class PinitAuthApplicationTests {
     void contextLoads() {
     }
 
+    @TestConfiguration
+    static class TestBeans {
+        @Bean
+        @Primary
+        JwtTokenProvider testJwtTokenProvider() {
+            return new JwtTokenProvider(TestKeys.privateKey(), TestKeys.publicKey(), "https://pinit.go-gradually.me", Duration.ofMinutes(5), Duration.ofDays(14));
+        }
+    }
 }
