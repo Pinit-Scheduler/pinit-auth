35404bf2ae0e62d436c1b6a9d975d65a2383b275
GoGradually <chkun3109@gmail.com>
2025-12-31 18:48:38 +0900

    fix: CORS 설정 수정 및 불필요한 코드 제거

--- src/main/java/me/gg/pinit/authenticate/config/SecurityConfig.java
+++ src/main/java/me/gg/pinit/authenticate/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import me.gg.pinit.infrastructure.jwt.RsaKeyProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.Customizer;
@@ -19,14 +18,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.time.Duration;
-import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -88,24 +83,5 @@ public class SecurityConfig {
         PrivateKey privateKey = RsaKeyProvider.loadPrivateKey(privateKeyPath);
         PublicKey publicKey = RsaKeyProvider.loadPublicKey(publicKeyPath);
         return new JwtTokenProvider(privateKey, publicKey, "https://pinit.go-gradually.me", Duration.ofMinutes(5), Duration.ofDays(14));
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource(@Value("${app.frontend-base-url}") String frontendBaseUrl) {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(frontendBaseUrl));
-        configuration.setAllowedMethods(List.of(
-                HttpMethod.GET.name(),
-                HttpMethod.POST.name(),
-                HttpMethod.PUT.name(),
-                HttpMethod.DELETE.name(),
-                HttpMethod.OPTIONS.name()
-        ));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 }
